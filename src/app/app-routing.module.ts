edb76829cb87665cb8c493ebb333f6d835feb771
Danny Estrada <danyestrada1999@gmail.com>
2020-01-29 21:16:23 -0500

    Interfaz Perfil de usuario
    es solo la interfaz, no es funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from "./auth/login/login.component";
+import { PerfilComponent } from './perfil/perfil.component';
 
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
+  { path: "perfil", component: PerfilComponent},
 ];
 
 @NgModule({
