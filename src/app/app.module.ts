fd52dd54a0cde1e072ac75597288741312aa2033
Sebas Rendon <sebasrg0505@gmail.com>
2020-01-29 21:47:50 -0500

    Botones de homepage sin carrusel
    Falta llenar el espacio blanco del home, pero es algo cosmetico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -22,8 +19,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
       [
         { path: '', component: HomeComponent},
       ]
-    ),
-    NgbModule
+    )
   ],
   exports: [HomeComponent],
   providers: [],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,35 +1,15 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
 
-@Component({selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.css']})
-export class HomeComponent {
-  images = ["car1.jpg", "car2.png", "car3.jpg"].map((n) => `assets/img/carousel/${n}`);
+  constructor() { }
 
-  paused = false;
-  unpauseOnArrow = false;
-  pauseOnIndicator = false;
-  pauseOnHover = true;
-
-  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
-
-  togglePaused() {
-    if (this.paused) {
-      this.carousel.cycle();
-    } else {
-      this.carousel.pause();
-    }
-    this.paused = !this.paused;
+  ngOnInit() {
   }
 
-  onSlide(slideEvent: NgbSlideEvent) {
-    if (this.unpauseOnArrow && slideEvent.paused &&
-      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
-      this.togglePaused();
-    }
-    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
-      this.togglePaused();
-    }
-  }
 }
-
