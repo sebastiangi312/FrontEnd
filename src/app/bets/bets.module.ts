174b92fae624d9b982c7d291b7e331ebc886050b
Santiago Rendon <santirg2000@gmail.com>
2020-02-17 21:44:58 -0500

    Minor stuff

--- src/app/bets/bets.module.ts
+++ src/app/bets/bets.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BetsListComponent } from './bets-list/bets-list.component';
+import { CreateLotteryComponent } from './bets-create/lottery/create-lottery/create-lottery.component'
 import { BetsRoutingModule } from './bets-routing.module';
-import { MaterialModule  } from '../material/material.module';
+import { MaterialModule } from '../material/material.module';
 import { MatTableModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [BetsListComponent],
+  declarations: [BetsListComponent, CreateLotteryComponent],
   imports: [
     CommonModule,
     BetsRoutingModule,
     MaterialModule,
-    MatTableModule
+    MatTableModule,
+    FormsModule
   ]
 })
 export class BetsModule { }
