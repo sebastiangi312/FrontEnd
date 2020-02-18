d6aaf48b377c2e2d5db06af159788ced9db74c0c
Sebastian Giraldo <sebastiangi312@gmail.com>
2020-02-17 23:56:38 -0500

    Bug Arreglado de danny

--- src/app/core/services/bets-list.service.ts
+++ src/app/core/services/bets-list.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
+import { Bet } from '../models/bet.model';
 
 const BACKEND_URL = environment.apiUrl + "";
 
