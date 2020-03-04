147dab532ce9dd235856a3887b7bd59ee4da06ca
Santiago Rendon <santirg2000@gmail.com>
2020-03-03 21:26:33 -0500

    Sprint 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://mplayapp-env.5mmgqmpbns.us-east-1.elasticbeanstalk.com/api'
+  apiUrl: 'https://mplay-app-api.herokuapp.com/api'
 };
