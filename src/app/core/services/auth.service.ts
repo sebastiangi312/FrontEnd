0d2018a2bf0528dd3550bb3b1eb214a1fec6ab1d
Santiago Rendon <santirg2000@gmail.com>
2020-03-03 21:51:51 -0500

    Sprint 3

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Subject } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 
-const BACKEND_URL = environment.apiUrl + '/user';
+const BACKEND_URL = environment.apiUrl + '/user/';
 
 @Injectable({
   providedIn: 'root'
