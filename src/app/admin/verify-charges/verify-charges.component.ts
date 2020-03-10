b8f79ab1484f83c85339ab564c2aa02e5ec7ff3c
Simón <sialvarezve@unal.edu.co>
2020-03-10 14:53:22 -0500

    Update verify-charges.component.ts

--- src/app/admin/verify-charges/verify-charges.component.ts
+++ src/app/admin/verify-charges/verify-charges.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { ProfileData } from "src/app/core/models/";
-import { FormGroup, Validators, FormControl } from "@angular/forms";
-import { Subscription } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { ProfileData } from 'src/app/core/models/';
+import { UsersService } from 'src/app/core/services/users.service';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
-import { ChargesService } from "../../core/services/charges.service";
-import { PageEvent } from "@angular/material";
-import { Charge } from "src/app/core/models/charge.model";
+import { User } from '../../core/models/user.model';
+import { ChargesService } from '../../core/services/charges.service';
+import { PageEvent } from '@angular/material';
+import { Charge } from 'src/app/core/models/charge.model';
 
 @Component({
-  selector: "app-verify-charges",
-  templateUrl: "./verify-charges.component.html",
-  styleUrls: ["./verify-charges.component.css"]
+  selector: 'app-verify-charges',
+  templateUrl: './verify-charges.component.html',
+  styleUrls: ['./verify-charges.component.css']
 })
 export class VerifyChargesComponent implements OnInit {
   isLoading = false;
@@ -18,24 +20,26 @@ export class VerifyChargesComponent implements OnInit {
 
   totalCharges = 0;
 
-  chargesPerPage = 10;
+  chargesPerPage = 5;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
   chargeId: string;
-  chargeUserName: string[] = [];
+  chargeUserName: string[];
   private chargesSub: Subscription;
 
-  constructor(public chargesService: ChargesService) {}
+  constructor(public chargesService: ChargesService) { }
 
   ngOnInit() {
     this.isLoading = true;
     this.chargesService.getCharges(this.chargesPerPage, this.currentPage);
+    this.chargeId = this.chargesService.getChargeId();
     this.chargesSub = this.chargesService
       .getChargeUpdateListener()
       .subscribe((chargeData: { charges: Charge[]; chargeCount: number }) => {
+        this.charges = chargeData.charges;
+        console.log(this.charges);
         this.isLoading = false;
         this.totalCharges = chargeData.chargeCount;
-        this.charges = chargeData.charges;
       });
   }
 
@@ -48,37 +52,29 @@ export class VerifyChargesComponent implements OnInit {
 
   onDelete(chargeId: string, idChargeToDelete: string) {
     this.isLoading = true;
-    this.chargesService.deleteCharge(chargeId, idChargeToDelete).subscribe(
-      () => {
-        this.chargesService.getCharges(this.chargesPerPage, this.currentPage);
-      },
-      () => {
-        this.isLoading = false;
-      }
-    );
+    this.chargesService.deleteCharge(chargeId, idChargeToDelete).subscribe(() => {
+      this.chargesService.getCharges(this.chargesPerPage, this.currentPage);
+    }, () => {
+      this.isLoading = false;
+    });
   }
 
   onAuthorize(idChargeToAuthorize: string) {
     this.isLoading = true;
-    this.chargesService.authorizeCharge(idChargeToAuthorize).subscribe(
-      () => {
-        this.chargesService.getCharges(this.chargesPerPage, this.currentPage);
-      },
-      () => {
-        this.isLoading = false;
-      }
-    );
+    this.chargesService.authorizeCharge(idChargeToAuthorize).subscribe(() => {
+      this.chargesService.getCharges(this.chargesPerPage, this.currentPage);
+    }, () => {
+      this.isLoading = false;
+    });
   }
 
   onDeauthorize(idChargeToAuthorize: string) {
     this.isLoading = true;
-    this.chargesService.deauthorizeCharge(idChargeToAuthorize).subscribe(
-      () => {
-        this.chargesService.getCharges(this.chargesPerPage, this.currentPage);
-      },
-      () => {
-        this.isLoading = false;
-      }
-    );
+    this.chargesService.deauthorizeCharge(idChargeToAuthorize).subscribe(() => {
+      this.chargesService.getCharges(this.chargesPerPage, this.currentPage);
+    }, () => {
+      this.isLoading = false;
+    });
   }
+
 }
